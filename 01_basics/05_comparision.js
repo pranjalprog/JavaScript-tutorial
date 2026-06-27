7711058c1e4ff989e9b3d434ecd9a878af5bb2a0
Pranjal Jain <pranjaljain19022005@gmail.com>
2026-06-27 11:34:47 +0530

    ok

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,28 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0); 
+console.log(null==0);
+console.log(null>=1);
+//js ne null ko 0 mai convert kr dia  - return true 
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+
+
+//js mai == and === ka alag working hota and >< inka alag working hota hai 
+// === => strict check means not check only value but check its datatype also 
+// for eg 
+console.log("2" == 2);
+console.log("2" === 2);
+
